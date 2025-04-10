b2615b344f5958889f6c134f50ddf64d43122b3b
MrMKN <knmuhammed2@gmail.com>
2025-04-10 12:30:26 +0530

    add technologies & change loader

--- js/app.js
+++ js/app.js
@@ -30,62 +30,24 @@
  * Table of Contents End
  * ------------------------------------------------ */
 
-gsap.registerPlugin(ScrollTrigger);
+// --------------------------------------------- //
+// Loader Start //
+// --------------------------------------------- //
 
-// --------------------------------------------- //
-// Loader & Loading Animation Start
-// --------------------------------------------- //
+const preloader = document.querySelector('#preloader');
+if (preloader) {
+    window.addEventListener('load', () => {
+        preloader.remove();
+    });
+}
+
+gsap.registerPlugin(ScrollTrigger);
 const content = document.querySelector('body');
 const imgLoad = imagesLoaded(content);
-const loadingWrap = document.querySelector('.loading-wrap');
-const loadingItems = loadingWrap.querySelectorAll('.loading__item');
-const fadeInItems = document.querySelectorAll('.loading__fade');
 
-function startLoader() {
-    let counterElement = document.querySelector(".loader__count .count__text");
-    let currentValue = 0;
-    function updateCounter() {
-        if (currentValue < 100) {
-            let increment = Math.floor(Math.random() * 10) + 1;
-            currentValue = Math.min(currentValue + increment, 100);
-            counterElement.textContent = currentValue;
-            let delay = Math.floor(Math.random() * 120) + 25;
-            setTimeout(updateCounter, delay);
-        }
-    }
-    updateCounter();
-}
-startLoader();
 
-imgLoad.on('done', instance => {
-    hideLoader();
-    pageAppearance();
-});
-
-function hideLoader() {
-    gsap.to(".loader__count", { duration: 0.8, ease: 'power2.in', y: "100%", delay: 1.8 });
-    gsap.to(".loader__wrapper", { duration: 0.8, ease: 'power4.in', y: "-100%", delay: 2.2 });
-    setTimeout(() => {
-        document.getElementById("loader").classList.add("loaded");
-    }, 3200);
-}
-
-function pageAppearance() {
-    gsap.set(loadingItems, { opacity: 0 })
-    gsap.to(loadingItems, {
-        duration: 1.1,
-        ease: 'power4',
-        startAt: { y: 120 },
-        y: 0,
-        opacity: 1,
-        delay: 0.8,
-        stagger: 0.05
-    }, '>-=1.1');
-    gsap.set(fadeInItems, { opacity: 0 });
-    gsap.to(fadeInItems, { duration: 0.8, ease: 'none', opacity: 1, delay: 3.2 });
-}
 // --------------------------------------------- //
-// Loader & Loading Animation End
+// Loader End
 // --------------------------------------------- //
 
 // --------------------------------------------- //
@@ -656,3 +618,4 @@ document.getElementById('color-switcher').addEventListener('click', () => {
 // --------------------------------------------- //
 // Color Switch End
 // --------------------------------------------- //
+
